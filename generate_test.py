7c9f5a8269cabedac943d12a10a5d7739ee4e8b3
мой зайчик <mojzajcik@MacBook-Pro-Guzel.local>
2025-05-21 08:18:53 -0700

    neww

---
+++ generate_test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Создаем тестовые данные
+data = {
+    "Name": ["Point1", "Point2", "Point3", "Point4", "Point5"],
+    "X": [1000.123, 2000.456, 3000.789, 4000.012, 5000.345],
+    "Y": [1500.234, 2500.567, 3500.890, 4500.123, 5500.456],
+    "Z": [500.345, 1000.678, 1500.901, 2000.234, 2500.567]
+}
+
+# Создаем DataFrame
+df = pd.DataFrame(data)
+
+# Сохраняем в Excel
+df.to_excel("test_coordinates.xlsx", index=False, engine='openpyxl')

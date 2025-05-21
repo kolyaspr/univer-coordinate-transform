8824ad55d405e30adaf719442c19abf7aaf99866
мой зайчик <mojzajcik@MacBook-Pro-Guzel.local>
2025-05-21 08:32:53 -0700

    comments

--- help.js
+++ help.js
@@ -1,3 +1,11 @@
+// venv
 // python3 -m venv venv
 // source venv/bin/activate
+
+// установка зависимостей 
 // pip install -r requirements.txt
+
+// запушить на гитхаб:
+// git add .
+// git commit -m "Initial commit" 
+// git push
